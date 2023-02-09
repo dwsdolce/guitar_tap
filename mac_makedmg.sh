4995d94ed04f5972e476de918dc32d47ca557eb3
David Smith <dws@dolcesfogato.com>
2023-02-09 11:26:16 -0800

    dmg creation update
    Made the image cleaner for background.

--- mac_makedmg.sh
+++ mac_makedmg.sh
@@ -4,18 +4,20 @@ test -f "Guitar Tap.dmg" && rm "Guitar Tap.dmg"
 # Create the dmg.
 echo "Creating DMG"
 create-dmg \
---volname "Guitar Tap 0.7 Install" \
---background "icons/guitar-tap-dmg.png" \
---window-pos 1 1 \
---icon "Guitar Tap.app" 190 360 \
---window-size 640 530 \
---icon-size 90 \
---app-drop-link 450 360 \
---format ULFO \
---hdiutil-verbose \
---volicon "icons/guitar-tap.icns" \
-"Guitar Tap.dmg" \
-"dist/"
+  --volname "Guitar Tap 0.7 Install" \
+  --background "icons/guitar-tap-dmg.png" \
+  --window-pos 1 1 \
+  --icon "Guitar Tap.app" 190 350 \
+  --window-size 640 535 \
+  --icon-size 110 \
+  --icon "Applications" 110 110 \
+  --hide-extension "Applications" \
+  --app-drop-link 450 360 \
+  --format ULFO \
+  --hdiutil-verbose \
+  --volicon "icons/guitar-tap.icns" \
+  "Guitar Tap.dmg" \
+  "dist/"
 
 echo "Signing DMG"
 /usr/bin/codesign -s "Developer ID Application: David Smith (43QHHT3XK2)" "Guitar Tap.dmg"
